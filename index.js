89b9022b9eb23d474dcd7604a92d00750f0acc9d
EunSungMoon <eunsung9647@gmail.com>
2021-07-25 15:20:54 +0900

    currentPage를 이벤트 함수에 걸리는지 확인. 꼭 성공시킨다

--- index.js
+++ index.js
@@ -13,12 +13,30 @@ const render = data => {
       for (let j = 0; j < 6; j++) {
         let createTd = document.createElement('td')
         createTd.id = `num${j + 1}`
-        createTr.appendChild(createTd)
+        createTr.appendChild(createTd);
       }
-      table.appendChild(createTr)
+      table.appendChild(createTr);
     }
   }
-  renderTable()
+  renderTable();
+
+  //페이징
+  const paging = () => {
+    let pageNum = document.querySelector('#pagingNum');
+    let startPage = `<a href = '#' id='start'>처음</a>`
+    let prevPage = `<a href = '#' id='prev'>이전</a>`
+    let nextPage = `<a href = '#' id="next">다음</a>`
+    let endPage = `<a href = '#' id="end">마지막</a>`
+
+    $(pageNum).append(startPage);
+    $(pageNum).append(prevPage);
+    for (let j = 0; j < totalPage; j++) {
+      pageNum.innerHTML += `<a href = '#' id= 'num${j + 1}'> ${j + 1} </a>`;
+    }
+    $(pageNum).append(nextPage);
+    $(pageNum).append(endPage);
+  }
+  paging();
 
   const renderData = (data, sidx, currentPage) => { //테이블에 json 값 넣기 sidx:10개씩 
 
@@ -29,21 +47,6 @@ const render = data => {
     let prevPage = currentPage - 1;
     let nextPage = currentPage + 1;
 
-    // if (startIndex < firstIndex) { //시작 인덱스< 첫번쨰 인덱스보다 작을 경우 같도록
-    //   startIndex = firstIndex;
-    // }
-    // console.log("비교" + startIndex);
-
-    // if (prevPage < 0) {
-    //   prevPage = currentPage
-    // }
-    // console.log("이전" + prevPage);
-
-    // if (nextPage > totalPage) {
-    //   nextPage = totalPage
-    // }
-    // console.log("다음" + nextPage);
-
     for (let i = 0; i < totalPage; i++) { //질문 json column index로 가져오는 법
 
       $('#' + String(i + 1)).children('#num1').text(data[i + sidx].num)
@@ -53,47 +56,23 @@ const render = data => {
       $('#' + String(i + 1)).children('#num5').text(data[i + sidx].type)
       $('#' + String(i + 1)).children('#num6').text(data[i + sidx].ip_address)
     }
-  }
 
-  renderData(data, 0 * 10, 1)
+    let pageSelector = document.querySelectorAll('a');
 
-  //페이징
-  const paging = () => {
-    let pageNum = document.querySelector('#pagingNum');
-    let startPage = `<a href = '#' id='1'>처음</a>`
-    let prevPage = `<a href = '#' id='prev'>이전</a>`
-    let nextPage = `<a href = '#' id="next">다음</a>`
-    let endPage = `<a href = '#' id="10">마지막</a>`
+    console.log("다음"+nextPage);
+    console.log("이전"+prevPage);
 
-    $(pageNum).append(startPage)
-    $(pageNum).append(prevPage)
-    for (let j = 0; j < 10; j++) {
-      pageNum.innerHTML += `<a href = '#' id= '${j + 1}'> ${j + 1} </a>`;
-    }
-    $(pageNum).append(nextPage)
-    $(pageNum).append(endPage)
-  }
-  paging()
-
-  let pageSelector = document.querySelectorAll('a');
-  pageSelector.forEach((target, currentPage) => { //페이징 이벤트
-    target.addEventListener('click', e => {
-
-      if(e.target.id=='prev'){
-        renderData(data,prevPage,currentPage)
-      }
-      else if(e.target.id=='next'){
-        renderData(data, nextPage,currentPage)
-      }
-        renderData(data, ((e.target.id) - 1) * dataPerPage, currentPage)
-    
-      //인덱스가 10개씩 되도록 안그럼 2페이지에 2번부터 나와
-      e.target.style.color = "red"
+    pageSelector.forEach((target, currentPage) => { //페이징 이벤트
+      target.addEventListener('click', e => {
+        renderData(data, ((e.target.innerText) - 1) * dataPerPage, (currentPage-1)); //인덱스가 10개씩 되도록 안그럼 2페이지에 2번부터 나와
+        console.log(e.target.id);
+        console.log((currentPage-1));
+        e.target.style.color = "red"
+      })
     })
-  })
-
-
-};
+  };
+  renderData(data, 0 * 10, 1);
+}
 
 //데이터를 부르는 역할
 const loader = url => {
